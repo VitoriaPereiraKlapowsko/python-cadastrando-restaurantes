efd76499200c9ba9611286985bc00825850506dd
VitoriaPereiraKlapowsko <vitoriapereiraklapowsko@gmail.com>
2024-02-24 17:42:59 -0300

    Refatorando partes do código para evitar a repetição

--- teste.py
+++ teste.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = ['Pizza', 'Sushi']
+
 def exibir_nome_do_programa():
     print('''
 ██████████████████████████████████████████████████████████████████████████
@@ -15,14 +17,36 @@ def exibir_opcoes():
     print('4. Sair')
     
 def finalizar_app(): #ao inves de usar function eu coloco def
-    os.system('cls') #importei a biblioteca OS para poder limpar o terminal após terminar de executar o programa
-    print('Finalizando o App\n')
+    exibir_subtitulo('Finalizando o App')
 
+def voltar_ao_menu_principal():
+    input('Digite uma tecla para voltar ao menu ')
+    main()
+    
 def opcao_invalida():
     print('Opção Inválida!!!\n')
-    input('Digite uma tecla para voltar ao menu principal')
-    main()
+    voltar_ao_menu_principal()
+    
+def exibir_subtitulo(texto):
+    os.system('cls') #importei a biblioteca OS para poder limpar o terminal após terminar de executar o programa
+    print(texto)
+    print()
+       
+def cadastrar_novo_restaurante():
+    exibir_subtitulo('Cadastro de novos restaurantes')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    restaurantes.append(nome_do_restaurante) #colocando os restaurantes cadastrados dentro da lista
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!!')
+    voltar_ao_menu_principal()
+    
+def listar_restaurantes():
+    exibir_subtitulo('Lista de novos restaurantes')
+    
+    for restaurante in restaurantes:
+        print(f'{restaurante}')
 
+    voltar_ao_menu_principal()
+    
 def escolher_opcoes():
     try: #vai servir para caso a pessoa tenha digitado uma letra ao invés de um número, ai ele tenta fazer isso se não ele pega a opção inválida
         opcao_escolhida = int(input('Escolha uma opção: ')) #Para a pessoa pode escrever e cursor ficar piscandinho, usa snake_case
@@ -31,9 +55,9 @@ def escolher_opcoes():
         #print(f'Você escolheu a opção {opcao_escolhida}')  ela se faz assim
 
         if opcao_escolhida == 1 : #Subistituir (){} por : e tirar os ()
-            print('Cadastrar Restaurante')
+            cadastrar_novo_restaurante()
         elif opcao_escolhida == 2 : #Elif é um else if
-            print('Listar Restaurantes')
+            listar_restaurantes()
         elif opcao_escolhida == 3 : 
             print('Ativar Restaurante')
         elif opcao_escolhida == 4 : 
@@ -49,5 +73,5 @@ def main():
     exibir_opcoes()
     escolher_opcoes()
     
-if __name__ == '__main__':
+if __name__ == '__main__':  #Aqui só estou declarando o meu Main e sim ele tem este padrão para ser declarado...
     main()
